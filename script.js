fa0f5a7e72796046c5c76f2ce5ea9bb42baef896
Code Sandbox <hello@codesandbox.io>
2023-01-12 07:28:25 +0000

    Deployment from Codesandbox.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // smooth scroll
-
+// testos
 const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
